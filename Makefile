f33146edab85f4813b4d52be80db29fe90f3837b
Paolo D'Incau <paolo.dincau@xpeppers.com>
2013-10-31 20:51:43 +0100

    Introducing tests

--- Makefile
+++ Makefile
@@ -1,16 +1,24 @@
 PROJECT = facebook_listener
 
+CT_SUITES = eunit http
+
 DEPS = cowboy jsx lager
 dep_cowboy = pkg://cowboy master
 dep_jsx = pkg://jsx master
 dep_lager = https://github.com/basho/lager.git master
 
-.PHONY: release clean-release
+# Standard targets.
+#include erlang.mk
 
 release: clean-release all
 	relx
 
 clean-release:
 	rm -rf _rel
+
+# Extra targets.
+autobahn: clean clean-deps deps app build-tests
+	@mkdir -p logs/
+	@$(CT_RUN) -suite autobahn_SUITE
 
 include erlang.mk
---
+++ test/http_SUITE.erl
@@ -0,0 +1,63 @@
+-module(http_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+
+%% ct.
+-export([all/0]).
+-export([groups/0]).
+-export([init_per_suite/1]).
+-export([end_per_suite/1]).
+-export([init_per_group/2]).
+-export([end_per_group/2]).
+
+%% Tests.
+-export([callback_with_expected_params/1,
+         callback_with_missing_params/1]).
+
+%% ct.
+all() ->
+    [{group, http}].
+
+groups() ->
+    Tests = [callback_with_expected_params, callback_with_missing_params],
+    [{http, [parallel], Tests}].
+
+init_per_suite(Config) ->
+    application:start(crypto),
+    application:start(cowlib),
+    application:start(ranch),
+    application:start(cowboy),
+    application:start(inets),
+    [].
+
+end_per_suite(_Config) ->
+    application:stop(inets),
+    application:stop(cowboy),
+    application:stop(ranch),
+    application:stop(cowlib),
+    application:stop(crypto),
+    ok.
+
+init_per_group(http, Config) ->
+    Transport = ranch_tcp,
+    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
+            {env, [{dispatch, init_dispatch(Config)}]},
+            {max_keepalive, 50},
+            {timeout, 500}]),
+    Port = ranch:get_port(http),
+    [Config].
+
+end_per_group(http, _Config) ->
+    ok.
+
+%% Dispatch configuration.
+init_dispatch(Config) ->
+    cowboy_router:compile([{"localhost", [{"/", toppage_handler, []}]}]).
+
+callback_with_missing_params(_Config) ->
+    {ok, {{"HTTP/1.1", 404, "Not Found"}, _Headers, _Body}} = httpc:request(get, {"http://localhost:8080", []}, [], []),
+    ok.
+
+callback_with_expected_params(_Config) ->
+    {ok, {{"HTTP/1.1", 200, "OK"}, _Headers, "mychallenge"}} = httpc:request(get, {"http://localhost:8080/?hub.mode=subscribe&hub.verify_token=token&hub.challenge=mychallenge", []}, [], []),
+    ok.
