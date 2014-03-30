2e5de542002df0e83e3a045c8b1fd2e73ef909d0
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-03-30 13:15:06 +0200

    uniforming port number in test scripts

--- scripts/curl_tests/subscription_verification.sh
+++ scripts/curl_tests/subscription_verification.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -v "http://localhost:8080/?hub.challenge=any_challenge&hub.mode=subscribe&hub.verify_token=token"
+curl -v "http://localhost:5498/?hub.challenge=any_challenge&hub.mode=subscribe&hub.verify_token=token"
--- scripts/curl_tests/update_verification.sh
+++ scripts/curl_tests/update_verification.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -v --data "{\"object\":\"user\",\"entry\":[{\"uid\":1335845740,\"changed_fields\":[\"name\",\"picture\"],\"time\":232323},{\"uid\":1234,\"changed_fields\":[\"friends\"],\"time\":232325}]}" -H "Content-Type:application/json" "http://localhost:8080/"
+curl -v --data "{\"object\":\"user\",\"entry\":[{\"uid\":1335845740,\"changed_fields\":[\"name\",\"picture\"],\"time\":232323},{\"uid\":1234,\"changed_fields\":[\"friends\"],\"time\":232325}]}" -H "Content-Type:application/json" "http://localhost:5498/"
