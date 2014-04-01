4acae8d7bcb75eaaeb4f03a4a4e2f119a949c297
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-01 23:59:54 +0200

    Recovering app_id and app_secret using app_name

--- src/fetcher.erl
+++ src/fetcher.erl
@@ -6,16 +6,35 @@
 
 -define(BASE_URL, "https://graph.facebook.com/815447318/friends?fields=name&access_token=").
 
-fetch(_AppName) ->
-    Address = lists:concat([?BASE_URL, access_token()]),
+fetch(AppName) ->
+    case application_credentials(AppName) of
+        {AppId, AppSecret} ->
+            Address = lists:concat([?BASE_URL, access_token(AppId, AppSecret)]),
+            do_fetch(Address),
+            case httpc:request(Address) of
+                {ok, {{_, 200, _}, _Headers, Body}} ->
+                    %% For now I just log the list of my friends
+                    lager:info("My Facebook friends are: ~p", [Body]);
+                Error ->
+                    lager:warning("Could fetch from url: ~p~n. Response was: ~p", [Error])
+            end;
+        _ ->
+            lager:warning("Undefined application name: ~p", [AppName])
+    end.
 
+application_credentials(_AppName) ->
+    %% For now this is super stubbed
+    {"app_id", "app_secret"}.
+
+access_token(AppId, AppSecret) ->
+    lists:concat([AppId, "|", AppSecret]).
+
+do_fetch(Address) ->
     case httpc:request(Address) of
         {ok, {{_, 200, _}, _Headers, Body}} ->
-            %% For now I just log the list of my friends
+             %% For now I just log the list of my friends
             lager:info("My Facebook friends are: ~p", [Body]);
         Error ->
             lager:warning("Could fetch from url: ~p~n. Response was: ~p", [Error])
     end.
 
-access_token() ->
-    lists:concat(["app_id", "|", "your_app_secret"]).
