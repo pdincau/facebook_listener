100af81d2cf7b79de3dd59e824612271b94b996b
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-01 23:47:49 +0200

    Recovering list of Paolo's friends using GraphAPI

--- src/fetcher.erl
+++ src/fetcher.erl
@@ -4,19 +4,18 @@
 
 -export([fetch/1]).
 
-fetch(AppName) ->
-    Address = lists:concat(["http://www.gazzetta.it/", binary_to_list(AppName)]),
+-define(BASE_URL, "https://graph.facebook.com/815447318/friends?fields=name&access_token=").
+
+fetch(_AppName) ->
+    Address = lists:concat([?BASE_URL, access_token()]),
 
     case httpc:request(Address) of
         {ok, {{_, 200, _}, _Headers, Body}} ->
-            %% For now I just log the title of a generic web page
-            Title = extract_title(Body),
-            lager:info("The webpage title is: ~p", [Title]);
+            %% For now I just log the list of my friends
+            lager:info("My Facebook friends are: ~p", [Body]);
         Error ->
             lager:warning("Could fetch from url: ~p~n. Response was: ~p", [Error])
     end.
 
-extract_title(Text) ->
-    A = string:str(Text, "<title>"),
-    B = string:str(Text, "</title>"),
-    string:substr(Text, A+7, B-A-7).
+access_token() ->
+    lists:concat(["app_id", "|", "your_app_secret"]).
