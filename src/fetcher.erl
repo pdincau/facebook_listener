146ddc0cc2b0ff92a087ae11e2b3f288ef232f75
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-02 00:02:26 +0200

    Removing legacy code

--- src/fetcher.erl
+++ src/fetcher.erl
@@ -10,14 +10,7 @@ fetch(AppName) ->
     case application_credentials(AppName) of
         {AppId, AppSecret} ->
             Address = lists:concat([?BASE_URL, access_token(AppId, AppSecret)]),
-            do_fetch(Address),
-            case httpc:request(Address) of
-                {ok, {{_, 200, _}, _Headers, Body}} ->
-                    %% For now I just log the list of my friends
-                    lager:info("My Facebook friends are: ~p", [Body]);
-                Error ->
-                    lager:warning("Could fetch from url: ~p~n. Response was: ~p", [Error])
-            end;
+            do_fetch(Address);
         _ ->
             lager:warning("Undefined application name: ~p", [AppName])
     end.
