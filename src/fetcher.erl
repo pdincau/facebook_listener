1e09045e401d15929f18e2cf606da6003c3fdc04
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-02 22:38:08 +0200

    Adding fetcher:entries_in/1 which implements trivial entries recovery from update

--- src/fetcher.erl
+++ src/fetcher.erl
@@ -2,13 +2,15 @@
 
 -compile([{parse_transform, lager_transform}]).
 
--export([fetch/1]).
+-export([fetch/2]).
 
 -define(BASE_URL, "https://graph.facebook.com/815447318/friends?fields=name&access_token=").
 
-fetch(AppName) ->
+fetch(AppName, Update) ->
     case application_credentials(AppName) of
         {AppId, AppSecret} ->
+            Entries = entries_in(Update),
+            lager:info("Entries in update are: ~p", [Entries]),
             Address = lists:concat([?BASE_URL, access_token(AppId, AppSecret)]),
             do_fetch(Address);
         _ ->
@@ -28,6 +30,13 @@ do_fetch(Address) ->
              %% For now I just log the list of my friends
             lager:info("My Facebook friends are: ~p", [Body]);
         Error ->
-            lager:warning("Could fetch from url: ~p~n. Response was: ~p", [Error])
+            lager:warning("Couldn't fetch from url: ~p~n. Response was: ~p", [Error])
     end.
 
+entries_in(Update) ->
+    %% For now I just match 'user" object
+    [{<<"object">>, Object = <<"user">>}, {<<"entry">>, Entries}] = Update,
+
+    lager:info("Extracted from update: ~p object: ~p and entries: ~p", [Update, Object, Entries]),
+
+    [{UId, Fields, Timestamp} || [{<<"uid">>, UId}, {<<"changed_fields">>, Fields}, {<<"time">>, Timestamp}] <- Entries].
