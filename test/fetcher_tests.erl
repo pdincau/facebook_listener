ec531e15d1afb37de398cbc5c6f80c6cb8a02e10
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-02 23:08:09 +0200

    Testing fetcher:entries_in/1 when update entries don't have object user

--- test/fetcher_tests.erl
+++ test/fetcher_tests.erl
@@ -1,11 +1,21 @@
 -module(fetcher_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-entries_test() ->
-    Update = user_update(),
+user_entries_test() ->
+    Update = update_with_user_entries(),
     Entries = fetcher:entries_in(Update),
 
     ?assertEqual([{1335845740, [<<"name">>,<<"picture">>], 232323}, {1234, [<<"friends">>], 232325}], Entries).
 
-user_update() ->
+not_user_entries_test() ->
+    Update = update_without_user_entries(),
+    Entries = fetcher:entries_in(Update),
+
+    ?assertEqual([], Entries).
+
+update_with_user_entries() ->
     [{<<"object">>,<<"user">>},{<<"entry">>,[[{<<"uid">>,1335845740},{<<"changed_fields">>,[<<"name">>,<<"picture">>]},{<<"time">>,232323}],[{<<"uid">>,1234},{<<"changed_fields">>,[<<"friends">>]},{<<"time">>,232325}]]}].
+
+
+update_without_user_entries() ->
+    [{<<"object">>,<<"page">>},{<<"entry">>,[[{<<"uid">>,1335845740},{<<"changed_fields">>,[<<"name">>,<<"picture">>]},{<<"time">>,232323}]]}].
