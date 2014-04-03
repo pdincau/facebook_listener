35bc160456a6704e5579c436c83c6e1a5f03987f
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-03 21:38:27 +0200

    Refactoring fetcher

--- src/fetcher.erl
+++ src/fetcher.erl
@@ -9,21 +9,19 @@
 fetch(AppName, Update) ->
     Entries = entries_in(Update),
     lager:info("Entries in update are: ~p", [Entries]),
-    Results = [do_fetch(AppName, UserId, Fields, Timestamp) || {UserId, Fields, Timestamp} <- Entries],	
+    Results = [fetch_entry(AppName, UserId, Fields, Timestamp) || {UserId, Fields, Timestamp} <- Entries],
     lager:info("Results are: ~p", [Results]).
 
 access_token(_AppName, _UserId) ->
     %% TODO: Implement token recovery here.
     <<"securitytoken">>.
 
-do_fetch(AppName, UserId, Fields, _Timestamp) ->
-    Url1 = binary:replace(?BASE_URL, <<"{objectid}">>, UserId),
+fetch_entry(AppName, UserId, Fields, _Timestamp) ->
+    Token = access_token(AppName, UserId),
     %% TODO: this will make a request only for 1st field. Must be rewritten
-    Url2 = binary:replace(Url1, <<"{field}">>, lists:nth(1, Fields)),
-    Url3 = binary:replace(Url2, <<"{token}">>, access_token(AppName, UserId)),
-    Url4 = binary:replace(Url3, <<"{params}">>, <<"limit=1">>),
-    case httpc:request(binary_to_list(Url4)) of
-        {ok, {{_, 200, _}, _Headers, Body}} -> 
+    Url = url_for(UserId, Fields, Token, <<"limit=1">>),
+    case httpc:request(Url) of
+        {ok, {{_, 200, _}, _Headers, Body}} ->
             Body;
         Error ->
             lager:warning("Couldn't fetch from url: ~p~n. Response was: ~p", [Error])
@@ -38,6 +36,13 @@ entries_in(Update) ->
         _ ->
             []
     end.
+
+url_for(UserId, Fields, Token, Params) ->
+    Url = binary:replace(?BASE_URL, <<"{objectid}">>, UserId),
+    Url1 = binary:replace(Url, <<"{field}">>, lists:nth(1, Fields)),
+    Url2 = binary:replace(Url1, <<"{token}">>, Token),
+    Url3 = binary:replace(Url2, <<"{params}">>, Params),
+    binary_to_list(Url3).
 
 -ifdef(TEST).
     -compile(export_all).
