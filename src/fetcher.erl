11ece3fbbcce3b9957a6569ef36337420bfffe3c
Paolo D'Incau <paolo.dincau@gmail.com>
2014-04-03 14:40:54 -0400

    Replacing application_crentials/2 with access_token/2, doing real update fetch

--- src/fetcher.erl
+++ src/fetcher.erl
@@ -4,31 +4,27 @@
 
 -export([fetch/2]).
 
--define(BASE_URL, "https://graph.facebook.com/815447318/friends?fields=name&access_token=").
+-define(BASE_URL, <<"https://graph.facebook.com/{objectid}/{field}?access_token={token}&{params}">>).
 
 fetch(AppName, Update) ->
-    case application_credentials(AppName) of
-        {AppId, AppSecret} ->
-            Entries = entries_in(Update),
-            lager:info("Entries in update are: ~p", [Entries]),
-            Address = lists:concat([?BASE_URL, access_token(AppId, AppSecret)]),
-            do_fetch(Address);
-        _ ->
-            lager:warning("Undefined application name: ~p", [AppName])
-    end.
+    Entries = entries_in(Update),
+    lager:info("Entries in update are: ~p", [Entries]),
+    Results = [do_fetch(AppName, UserId, Fields, Timestamp) || {UserId, Fields, Timestamp} <- Entries],	
+    lager:info("Results are: ~p", [Results]).
 
-application_credentials(_AppName) ->
-    %% For now this is super stubbed
-    {"app_id", "app_secret"}.
+access_token(_AppName, _UserId) ->
+    %% TODO: Implement token recovery here.
+    <<"securitytoken">>.
 
-access_token(AppId, AppSecret) ->
-    lists:concat([AppId, "|", AppSecret]).
-
-do_fetch(Address) ->
-    case httpc:request(Address) of
-        {ok, {{_, 200, _}, _Headers, Body}} ->
-             %% For now I just log the list of my friends
-            lager:info("My Facebook friends are: ~p", [Body]);
+do_fetch(AppName, UserId, Fields, _Timestamp) ->
+    Url1 = binary:replace(?BASE_URL, <<"{objectid}">>, UserId),
+    %% TODO: this will make a request only for 1st field. Must be rewritten
+    Url2 = binary:replace(Url1, <<"{field}">>, lists:nth(1, Fields)),
+    Url3 = binary:replace(Url2, <<"{token}">>, access_token(AppName, UserId)),
+    Url4 = binary:replace(Url3, <<"{params}">>, <<"limit=1">>),
+    case httpc:request(binary_to_list(Url4)) of
+        {ok, {{_, 200, _}, _Headers, Body}} -> 
+            Body;
         Error ->
             lager:warning("Couldn't fetch from url: ~p~n. Response was: ~p", [Error])
     end.
@@ -38,8 +34,7 @@ entries_in(Update) ->
     case Update of
         [{<<"object">>, Object = <<"user">>}, {<<"entry">>, Entries}] ->
             lager:info("Extracted from update: ~p object: ~p and entries: ~p", [Update, Object, Entries]),
-
-            [{UId, Fields, Timestamp} || [{<<"uid">>, UId}, {<<"changed_fields">>, Fields}, {<<"time">>, Timestamp}] <- Entries];
+            [{UId, Fields, Timestamp} ||  [{<<"uid">>, UId}, {<<"id">>, _Id}, {<<"time">>, Timestamp}, {<<"changed_fields">>, Fields}] <- Entries];
         _ ->
             []
     end.
