61a45aba288d1bc3fd081e6bc49bec98a8afe9f7
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-08 21:45:50 +0200

    Trivial implementation of queue for bc

--- src/queue_bc.erl
+++ src/queue_bc.erl
@@ -2,6 +2,8 @@
 
 -behaviour(gen_server).
 
+-compile([{parse_transform, lager_transform}]).
+
 %% API
 -export([start_link/0]).
 
@@ -11,17 +13,31 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, {client}).
 
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({local, queue}, ?MODULE, [], []).
 
 init([]) ->
-    {ok, #state{}}.
+    % TODO: make init send gen_cast message to setup connection
+    State = case memcached:connect("127.0.0.1", 11211) of
+        {ok, Client} ->
+            #state{client=Client};
+        _Error ->
+            %% TODO: send message to self in order to retry connection
+            #state{client=undefined}
+    end,
+    {ok, State}.
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
+
+handle_cast({push, Msg}, #state{client=Client} = State) ->
+    lager:info("New message is: ~p", [Msg]),
+    %% TODO: handle errors here
+    ok = memcached:set(Client, "Hello", Msg),
+    {noreply, State};
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
