4bd5bc15847ccb772a85402d5f24f3becf46119c
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-12 12:07:06 +0200

    Removing unused macro

--- src/repo_mem.erl
+++ src/repo_mem.erl
@@ -11,8 +11,6 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
         terminate/2, code_change/3]).
 
--define(IDENTIFIER, <<"{app_name}:facebook:{user_id}">>).
-
 -record(state, {}).
 
 start_link() ->
