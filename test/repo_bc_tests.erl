33c6d84dbf400baf101baa837d4dcc90cf05ce64
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-04-17 14:18:42 +0200

    Removing files which should not be anymore in basic repo

--- test/repo_bc_tests.erl
+++
@@ -1,7 +0,0 @@
--module(repo_bc_tests).
--include_lib("eunit/include/eunit.hrl").
-
-identifier_test() ->
-    Identifier = repo_bc:identifier(<<"an_app_name">>, <<"a_user_id">>),
-
-    ?assertEqual("an_app_name:facebook:a_user_id", Identifier).
